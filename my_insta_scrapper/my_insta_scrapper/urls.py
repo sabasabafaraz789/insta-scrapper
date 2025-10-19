90208ca68f1d404e63201b439cfbae4d03509c13
Saba Faraz <farazsaba96@gmail.com>
2025-10-19 07:30:20 -0700

    The Selenium.py and BeautifulSoup4.py files executed successfully, and the result files were generated in the instagram_tool directory

--- my_insta_scrapper/my_insta_scrapper/urls.py
+++ my_insta_scrapper/my_insta_scrapper/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+
     path('', include('scrapper.urls')),
-    path('admin/', admin.site.urls),
+    # path('', include('scrapper.urls')),
+
+    # path('admin/', admin.site.urls),
 ]
 
 
