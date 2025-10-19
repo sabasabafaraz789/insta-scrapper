d76914a498056589082bfba1922116b6fbb1cff3
Saba Faraz <farazsaba96@gmail.com>
2025-10-19 05:48:40 -0700

    Instagram_tool dir added

--- my_insta_scrapper/scrapper/views.py
+++ my_insta_scrapper/scrapper/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Scrapper
+
+#def scrapper(request):
+ #   return HttpResponse("Hello world!")
+
+
+
+#def scrapper(request):
+  #template = loader.get_template('frontend.html')
+  #return HttpResponse(template.render())
+
+
+
+def scrapper(request):
+  myscrappers = Scrapper.objects.all().values()
+  template = loader.get_template('frontend.html')
+  context = {
+    'myscrappers': myscrappers,
+  }
+  return HttpResponse(template.render(context, request))
